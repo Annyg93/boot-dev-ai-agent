ee45b9f7c0c8c5b158cc4b1101ed056d61f5e3ab
Annyg93 <andrenygaard@gmail.com>
2026-05-19 13:09:14 +0200

    feat: initial project setup & implemented api call & user input

---
+++ main.py
@@ -0,0 +1,38 @@
+import os
+import argparse
+
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    if not api_key:
+        raise RuntimeError("api key not found")
+    
+    parser = argparse.ArgumentParser(description="AI code assistant")
+    parser.add_argument("user_prompt", type=str, help="User prompt to Gemini")
+    args = parser.parse_args()
+
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=messages,
+    )
+
+    if not response.usage_metadata:
+        raise RuntimeError("api request failed")
+    
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")    
+    print("Response:")
+    print(response.text)
+
+
+if __name__ == "__main__":
+    main()
